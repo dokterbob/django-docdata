9a4d29cda6d8c4942a59854bc18ee65a8528dbc5
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-03-17 16:46:03 +0100

    Status change view and removed other, less relevant views

--- docdata/models.py
+++ docdata/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 
 from docdata.interface import PaymentInterface
-from docdata.settings import MERCHANT_NAME, MERCHANT_PASSWORD, DEBUG, PROFILE
+from docdata.settings import MERCHANT_NAME, MERCHANT_PASSWORD, DEBUG, \
+                             PROFILE, TRANSACTION_ID_PREFIX
 
 
 class PaymentCluster(models.Model):
     """ Payment cluster model. """
 
-    def get_transaction_id(self):
+    def _get_transaction_id(self):
         """ Generate a transaction id from this cluster's public key. """
 
         assert self.pk, 'No public key available for unique reference'
 
-        if DEBUG:
-            return 'docdata-test-%d' % self.pk
-        else:
-            return 'docdata-%d' % self.pk
+        return '%s-%d' % (TRANSACTION_ID_PREFIX, self.pk)
+
+    @classmethod
+    def get_by_transaction_id(cls, transaction_id):
+        """ Get the payment cluster belonging to a specific transaction_id. """
+        pk = transaction_id[len(TRANSACTION_ID_PREFIX)+1:]
+        return cls.objects.get(pk=pk)
 
     def __init__(self, *args, **kwargs):
         """ Make sure we have an interface available. """
@@ -29,7 +33,7 @@ class PaymentCluster(models.Model):
 
         data = {'merchant_name': MERCHANT_NAME,
                 'merchant_password': MERCHANT_PASSWORD,
-                'merchant_transaction_id': self.get_transaction_id(),
+                'merchant_transaction_id': self._get_transaction_id(),
                 'profile': PROFILE,}
 
         data.update(kwargs)
--- docdata/views.py
+++ docdata/views.py
@@ -1,12 +1,30 @@
-from django.http import HttpResponse
+import logging
+logger = logging.getLogger(__name__)
+
+from django.http import HttpResponse, Http404
+
+from docdata.models import PaymentCluster
 
 
 def status_change(request):
     """ Update URL should have ?merchant_transaction_id=<id> set. """
-    merchant_transaction_id = request.GET.get('merchant_transaction_id')
+    transaction_id = request.GET.get('merchant_transaction_id')
+
+    logger.debug('Received status change message for transaction_id %s',
+                 transaction_id)
+
+    try:
+        payment_cluster = \
+            PaymentCluster.get_by_transaction_id(transaction_id)
+    except PaymentCluster.ObjectDoesNotExist:
+        logger.warning('Status change for payment cluster with merchant \
+                        transaction_id %s not matched', transaction_id)
+        raise Http404
+
+    payment_cluster.update_status()
+
+    logger.debug('Status for payment with transaction_id %s updated',
+                 transaction_id)
 
     # Allways return a 200
     return HttpResponse('')
-
-def user_feedback(request, status):
-    raise NotImplementedError
