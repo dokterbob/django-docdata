690add9d01b62e7144eb1afb5eed2662099c6963
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-02-10 11:49:45 +0100

    PEP8 cleanup.

--- setup.py
+++ setup.py
@@ -18,16 +18,16 @@ except:
     LICENSE = None
 
 setup(
-    name = 'django-docdata',
-    version = '0.2',
+    name='django-docdata',
+    version='0.2',
     description='Python/Django client to the Docdata payment system.',
-    long_description = README,
-    install_requires = REQUIREMENTS,
-    author = 'Mathijs de Bruin',
-    author_email = 'mathijs@mathijsfietst.nl',
-    license = LICENSE,
-    url = 'http://github.com/dokterbob/django-docdata/',
-    packages = find_packages(),
+    long_description=README,
+    install_requires=REQUIREMENTS,
+    author='Mathijs de Bruin',
+    author_email='mathijs@mathijsfietst.nl',
+    license=LICENSE,
+    url='http://github.com/dokterbob/django-docdata/',
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -39,8 +39,8 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Environment :: Web Environment',
     ],
-    test_suite = 'setuptest.setuptest.SetupTestSuite',
-    tests_require = (
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
         'django-setuptest',
         'argparse',  # apparently needed by django-setuptest on python 2.6
     ),
